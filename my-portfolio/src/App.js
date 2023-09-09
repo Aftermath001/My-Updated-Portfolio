c08ef659c53160ae366219f5639e13b9f2a84132
Aftermath001 <alvinasiachi@gmail.com>
2023-09-09 17:21:37 +0300

    Intro part-1

--- my-portfolio/src/App.js
+++ my-portfolio/src/App.js
@@ -1,9 +1,11 @@
+import Intro from './components/NavBar/Intro/Intro';
 import Navbar from './components/NavBar/Navbar/Navbar';
 
 function App() {
   return (
     <div className="App">
       <Navbar/>
+      <Intro />
     </div>
   );
 }
--- my-portfolio/src/components/NavBar/Intro/Intro.js
+++ my-portfolio/src/components/NavBar/Intro/Intro.js
@@ -1,8 +1,22 @@
 import React from 'react'
 import './intro.css'
+import { Link } from 'react-scroll'
+import WorkIcon from '@mui/icons-material/Work';
+import  introPic from '../../../assets/Bg.png'
+
 function Intro() {
   return (
-    <div>Intro</div>
+    <section id="intro">
+        <div className='introContent'>
+            <span className='hello'>Hello,</span>
+            <span className='introText'>I'm <span className='introName'>Adams</span> <br />Software Developer</span>
+            <p className='introPara'>I am a skilled developer with experience in developing visually appealing and user friendly websites and applications</p>
+            <Link><button className='btn'><i><WorkIcon/></i> Hire Me</button></Link>
+        </div>
+
+        <img src={introPic} alt='Profile' className='bg'/>
+
+    </section>
   )
 }
 
